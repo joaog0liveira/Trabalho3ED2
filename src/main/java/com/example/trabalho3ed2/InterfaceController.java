1902b883172fba34c484daf02cf89d644ac89bce
Breno Reis de Oliveria <119299485+AldrichGE@users.noreply.github.com>
2023-12-02 19:22:49 -0300

    FrontEnd

--- src/main/java/com/example/trabalho3ed2/InterfaceController.java
+++ src/main/java/com/example/trabalho3ed2/InterfaceController.java
@@ -21,92 +21,96 @@ public class InterfaceController {
 
     private Map<String, Map<String, Integer>> graph;
 
-    public void initialize() {
-        // Inicializar o grafo ao iniciar a aplicação
-        graph = readGraphFromFile("D:\\Developer\\Trabalho3ED2\\src\\main\\java\\com\\example\\trabalho3ed2\\cidades.txt");
+  // Método responsável por inicializar o grafo ao iniciar a aplicação
+  public void initialize() {
+    graph = readGraphFromFile("C:\\Users\\Breno Reis\\Documents\\Angular\\ProjetoAngular\\Trabalho3ED2\\src\\main\\java\\com\\example\\trabalho3ed2\\cidades.txt");
 
-        // Preencher ComboBox com as cidades
-        startCityComboBox.getItems().addAll(graph.keySet());
-        endCityComboBox.getItems().addAll(graph.keySet());
+    // Preencher ComboBox com as cidades
+    startCityComboBox.getItems().addAll(graph.keySet());
+    endCityComboBox.getItems().addAll(graph.keySet());
+  }
+
+  // Método acionado para calcular a menor distância entre cidades selecionadas
+  @FXML
+  private void calculateShortestDistance() {
+    String startCity = startCityComboBox.getValue();
+    String endCity = endCityComboBox.getValue();
+
+    if (startCity != null && endCity != null) {
+      int shortestDistance = dijkstra(graph, startCity, endCity);
+      resultLabel.setText(String.valueOf(shortestDistance));
+    } else {
+      resultLabel.setText("Selecione as cidades.");
+    }
+  }
+
+  // Método para ler um grafo de um arquivo e construir a estrutura de dados correspondente
+  public static Map<String, Map<String, Integer>> readGraphFromFile(String filename) {
+    Map<String, Map<String, Integer>> graph = new HashMap<>();
+
+    try (Scanner scanner = new Scanner(new File(filename))) {
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        String[] parts = line.split(",");
+        String source = parts[0].trim();
+        String destination = parts[1].trim();
+        int weight = Integer.parseInt(parts[2].trim());
+
+        graph.computeIfAbsent(source, k -> new HashMap<>());
+        graph.computeIfAbsent(destination, k -> new HashMap<>());
+
+        graph.get(source).put(destination, weight);
+        graph.get(destination).put(source, weight);
+      }
+    } catch (FileNotFoundException e) {
+      System.err.println("Erro ao ler o arquivo: " + e.getMessage());
     }
 
-    @FXML
-    private void calculateShortestDistance() {
-        String startCity = startCityComboBox.getValue();
-        String endCity = endCityComboBox.getValue();
+    return graph;
+  }
 
-        if (startCity != null && endCity != null) {
-            int shortestDistance = dijkstra(graph, startCity, endCity);
-            resultLabel.setText(String.valueOf(shortestDistance));
-        } else {
-            resultLabel.setText("Selecione as cidades.");
+  // Algoritmo de Dijkstra para encontrar a menor distância entre dois pontos em um grafo
+  public static int dijkstra(Map<String, Map<String, Integer>> graph, String start, String end) {
+    Map<String, Integer> distances = new HashMap<>();
+    for (String node : graph.keySet()) {
+      distances.put(node, Integer.MAX_VALUE);
+    }
+    distances.put(start, 0);
+
+    Set<String> unvisitedNodes = new HashSet<>(graph.keySet());
+
+    while (!unvisitedNodes.isEmpty()) {
+      String currentNode = getMinDistanceNode(distances, unvisitedNodes);
+      unvisitedNodes.remove(currentNode);
+
+      for (Map.Entry<String, Integer> neighborEntry : graph.get(currentNode).entrySet()) {
+        String neighbor = neighborEntry.getKey();
+        int weight = neighborEntry.getValue();
+
+        int potentialRoute = distances.get(currentNode) + weight;
+        if (potentialRoute < distances.get(neighbor)) {
+          distances.put(neighbor, potentialRoute);
         }
+      }
     }
 
-    public static Map<String, Map<String, Integer>> readGraphFromFile(String filename) {
-        Map<String, Map<String, Integer>> graph = new HashMap<>();
+    return distances.get(end);
+  }
 
-        try (Scanner scanner = new Scanner(new File(filename))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(",");
-                String source = parts[0].trim();
-                String destination = parts[1].trim();
-                int weight = Integer.parseInt(parts[2].trim());
+  // Método auxiliar para obter o nó com a menor distância na execução do algoritmo de Dijkstra
+  private static String getMinDistanceNode(Map<String, Integer> distances, Set<String> unvisitedNodes) {
+    String minDistanceNode = null;
+    int minDistance = Integer.MAX_VALUE;
 
-                graph.computeIfAbsent(source, k -> new HashMap<>());
-                graph.computeIfAbsent(destination, k -> new HashMap<>());
-
-                graph.get(source).put(destination, weight);
-                graph.get(destination).put(source, weight);
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
-            // Você pode optar por lançar uma exceção personalizada ou tratar de outra forma.
-        }
-
-        return graph;
+    for (String node : unvisitedNodes) {
+      int distance = distances.get(node);
+      if (distance < minDistance) {
+        minDistance = distance;
+        minDistanceNode = node;
+      }
     }
 
-    public static int dijkstra(Map<String, Map<String, Integer>> graph, String start, String end) {
-        Map<String, Integer> distances = new HashMap<>();
-        for (String node : graph.keySet()) {
-            distances.put(node, Integer.MAX_VALUE);
-        }
-        distances.put(start, 0);
-
-        Set<String> unvisitedNodes = new HashSet<>(graph.keySet());
-
-        while (!unvisitedNodes.isEmpty()) {
-            String currentNode = getMinDistanceNode(distances, unvisitedNodes);
-            unvisitedNodes.remove(currentNode);
-
-            for (Map.Entry<String, Integer> neighborEntry : graph.get(currentNode).entrySet()) {
-                String neighbor = neighborEntry.getKey();
-                int weight = neighborEntry.getValue();
-
-                int potentialRoute = distances.get(currentNode) + weight;
-                if (potentialRoute < distances.get(neighbor)) {
-                    distances.put(neighbor, potentialRoute);
-                }
-            }
-        }
-
-        return distances.get(end);
-    }
-
-    private static String getMinDistanceNode(Map<String, Integer> distances, Set<String> unvisitedNodes) {
-        String minDistanceNode = null;
-        int minDistance = Integer.MAX_VALUE;
-
-        for (String node : unvisitedNodes) {
-            int distance = distances.get(node);
-            if (distance < minDistance) {
-                minDistance = distance;
-                minDistanceNode = node;
-            }
-        }
-
-        return minDistanceNode;
-    }
+    return minDistanceNode;
+  }
 }
+
